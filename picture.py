3b518cc9d51ada8705aabc547376fc58b386a857
Artem <ares.preobrazhenskiy@gmail.com>
2024-12-15 01:17:01 +0300

    Update picture.py

--- picture.py
+++ picture.py
@@ -2,52 +2,44 @@ from PIL import Image
 
 img = Image.open("new.jpg")
 red_channel,green_channel, blue_channel = img.split()
-red_channel.save("red_channel.jpg")
-green_channel.save("green_channel.jpg") 
-blue_channel.save("blue_channel.jpg")
 
-img = Image.open("red_channel.jpg")
+
+
 left = 50
 top = 0
 right = 600
 bottom = 522
-img1 = img.crop((left, top, right, bottom))
+img1 = red_channel.crop((left, top, right, bottom))
 
 left = 25
 top = 0
 right = 575
 bottom = 522
-img2 = img.crop((left, top, right, bottom))
-
+img2 = red_channel.crop((left, top, right, bottom))
 img_red = Image.blend(img1, img2, 0.5)
 
 
 
-
-
-img = Image.open("blue_channel.jpg")
 left = 0
 top = 0
 right = 550
 bottom = 522
-img1 = img.crop((left, top, right, bottom))
+img1 = blue_channel.crop((left, top, right, bottom))
 
 left = 25
 top = 0
 right = 575
 bottom = 522
-img2 = img.crop((left, top, right, bottom))
-
+img2 = blue_channel.crop((left, top, right, bottom))
 img_blue = Image.blend(img1, img2, 0.5)
 
 
 
-img = Image.open("green_channel.jpg")
 left =25
 top = 0
 right = 575
 bottom = 522
-img_green = img.crop((left, top, right, bottom))
+img_green = green_channel.crop((left, top, right, bottom))
 
 new_image = Image.merge("RGB", (img_red, img_green, img_blue))
 new_image.show()
